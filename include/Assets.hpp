8856fde281d8f25c89ee9addde5402d30ff2e6d0
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-08-24 20:12:29 -0600

    Added PhysFS

---
+++ include/Assets.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <physfs.h>
+
+class Assets
+{
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <stdexcept>
 
+#include <physfs.h>
+
 #include "ShadowlightEngine.hpp"
 
-int main()
+int main(int argc, const char* argv[])
 {
+    PHYSFS_init(argv[0]);
+    
     try {
         ShadowlightEngine sle;
         sle.run();
     } catch(const std::exception& e) {
         std::cerr << e.what() << std::endl;
+        PHYSFS_deinit();
         return 1;
     }
 
+    PHYSFS_deinit();
     return 0;
 }
