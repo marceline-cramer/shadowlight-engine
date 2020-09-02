dd1cee391df001a8c773426b52c79f44ad59576f
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-09-01 18:10:16 -0600

    Created Vulkan logical device

--- src/bindings/VulkanBinding.cpp
+++ src/bindings/VulkanBinding.cpp
@@ -14,9 +14,17 @@ VulkanBinding::VulkanBinding()
                               SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
     if(!window) {
         throw std::runtime_error("Failed to create SDL window");
-    }    
+    }
 
     // Setup Vulkan
+    createInstance();
+    setupDebugMessenger();
+    pickPhysicalDevice();
+    createLogicalDevice();
+}
+
+void VulkanBinding::createInstance()
+{
     uint32_t extensionCount;
     SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr);
     std::vector<const char *> extensionNames(extensionCount);
@@ -49,8 +57,103 @@ VulkanBinding::VulkanBinding()
     }
 }
 
+void VulkanBinding::setupDebugMessenger()
+{
+    // TODO Validation layers
+}
+
+void VulkanBinding::pickPhysicalDevice()
+{
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+    if(deviceCount == 0) {
+        throw std::runtime_error("No Vulkan physical devices found");
+    }
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    // TODO Rank physical device support?
+    for(const auto& device : devices) {
+        if(isDeviceSuitable(device)) {
+            physicalDevice = device;
+            break;
+        }
+    }
+
+    if(physicalDevice == VK_NULL_HANDLE) {
+        throw std::runtime_error("Failed to find suitable Vulkan physical device");
+    }
+}
+
+QueueFamilyIndices VulkanBinding::findQueueFamilies(VkPhysicalDevice device)
+{
+    QueueFamilyIndices indices;
+
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily = i;
+            indices.hasGraphicsFamily = true;
+        }
+
+        if(indices.isComplete()) break;
+
+        i++;
+    }
+
+    return indices;
+}
+
+bool VulkanBinding::isDeviceSuitable(VkPhysicalDevice device)
+{
+    // TODO Add support for preferred physical device
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return indices.hasGraphicsFamily;
+}
+
+void VulkanBinding::createLogicalDevice()
+{
+    QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+    float queuePriority = 1.0f;
+
+    VkDeviceQueueCreateInfo queueCreateInfo{
+        .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        .queueFamilyIndex = indices.graphicsFamily,
+        .queueCount = 1,
+        .pQueuePriorities = &queuePriority
+    };
+
+    VkPhysicalDeviceFeatures deviceFeatures{};
+
+    VkDeviceCreateInfo createInfo{
+        .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        .queueCreateInfoCount = 1,
+        .pQueueCreateInfos = &queueCreateInfo,
+        // TODO Validation layers
+        .enabledLayerCount = 0,
+        .enabledExtensionCount = 0,
+        .pEnabledFeatures = &deviceFeatures
+    };
+
+    if(vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create Vulkan logical device");
+    }
+
+    vkGetDeviceQueue(device, indices.graphicsFamily, 0, &graphicsQueue);
+}
+
 VulkanBinding::~VulkanBinding()
 {
+    vkDestroyDevice(device, nullptr);
     vkDestroyInstance(instance, nullptr);
 
     SDL_DestroyWindow(window);
--- src/bindings/VulkanBinding.hpp
+++ src/bindings/VulkanBinding.hpp
@@ -9,6 +9,16 @@
 
 #include "bindings/Binding.hpp"
 
+struct QueueFamilyIndices
+{
+    uint32_t graphicsFamily;
+    bool hasGraphicsFamily = false;
+
+    bool isComplete() {
+        return hasGraphicsFamily;
+    }
+};
+
 class VulkanBinding : public Binding
 {
 public:
@@ -17,9 +27,21 @@ public:
 
     void update();
 private:
+    // Convenience functions
+    void createInstance();
+    void setupDebugMessenger();
+    void pickPhysicalDevice();
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice);
+    bool isDeviceSuitable(VkPhysicalDevice);
+    void createLogicalDevice();
+
     // SDL structures
     SDL_Window* window;
 
     // Vulkan data
     VkInstance instance;
+    VkPhysicalDevice physicalDevice;
+    VkDevice device;
+
+    VkQueue graphicsQueue;
 };
