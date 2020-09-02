e199071f693ff103ac34cbd6c97483607c6ef0bb
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-09-01 21:08:45 -0600

    Create swapchain image views

--- src/bindings/VulkanBinding.cpp
+++ src/bindings/VulkanBinding.cpp
@@ -40,10 +40,15 @@ VulkanBinding::VulkanBinding(Window* _window)
     pickPhysicalDevice();
     createLogicalDevice();
     createSwapchain();
+    createImageViews();
 }
 
 VulkanBinding::~VulkanBinding()
 {
+    for(auto imageView : swapChainImageViews) {
+        vkDestroyImageView(device, imageView, nullptr);
+    }
+
     vkDestroySwapchainKHR(device, swapChain, nullptr);
 
     if(enableValidationLayers) {
@@ -402,6 +407,37 @@ void VulkanBinding::createSwapchain()
 
     swapChainImageFormat = surfaceFormat.format;
     swapChainExtent = extent;
+}
+
+void VulkanBinding::createImageViews()
+{
+    swapChainImageViews.resize(swapChainImages.size());
+
+    for(size_t i = 0; i < swapChainImages.size(); i++) {
+        VkImageViewCreateInfo createInfo{
+            .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+            .image = swapChainImages[i],
+            .viewType = VK_IMAGE_VIEW_TYPE_2D,
+            .format = swapChainImageFormat,
+            .components = {
+                .r = VK_COMPONENT_SWIZZLE_IDENTITY,
+                .g = VK_COMPONENT_SWIZZLE_IDENTITY,
+                .b = VK_COMPONENT_SWIZZLE_IDENTITY,
+                .a = VK_COMPONENT_SWIZZLE_IDENTITY
+            },
+            .subresourceRange = {
+                .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                .baseMipLevel = 0,
+                .levelCount = 1,
+                .baseArrayLayer = 0,
+                .layerCount = 1
+            }
+        };
+
+        if(vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create swap chain image views");
+        }
+    }
 }
 
 void VulkanBinding::update()
--- src/bindings/VulkanBinding.hpp
+++ src/bindings/VulkanBinding.hpp
@@ -66,6 +66,7 @@ private:
     VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>&);
     VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR&);
     void createSwapchain();
+    void createImageViews();
 
     // Bindings
     Window* window;
@@ -83,6 +84,7 @@ private:
     
     VkSwapchainKHR swapChain;
     std::vector<VkImage> swapChainImages;
+    std::vector<VkImageView> swapChainImageViews;
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
 };
