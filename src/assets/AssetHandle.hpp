9caa90f61317b91d347d12eed1a7fe3c334dde56
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-09-15 12:53:50 -0600

    Fix AssetHandle unloading multiple times

--- src/assets/AssetHandle.hpp
+++ src/assets/AssetHandle.hpp
@@ -56,11 +56,11 @@ void AssetHandle<T>::deref()
     if(asset) {
         if(asset->refCount > 0) {
             asset->refCount--;
-        }
 
-        // TODO Use reaper in AssetPool so that assets are maintained
-        if(asset->refCount <= 0) {
-            asset->unload();
+            // TODO Use reaper in AssetPool so that assets are maintained
+            if(asset->refCount == 0) {
+                asset->unload();
+            }
         }
 
         asset = nullptr;
