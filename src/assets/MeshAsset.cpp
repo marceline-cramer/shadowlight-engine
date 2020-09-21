990a35b03e9e9b84342fbb077a8f326c41d26881
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-09-20 21:12:18 -0600

    Actually use MeshAsset indices

--- src/assets/MeshAsset.cpp
+++ src/assets/MeshAsset.cpp
@@ -91,7 +91,7 @@ void MeshAsset::createIndexBuffer()
     vkUnmapMemory(vk->device, stagingBufferMemory);
 
     // Create and copy index buffer
-    vk->createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+    vk->createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
 
     VkCommandBuffer commandBuffer = vk->beginSingleTimeCommands();
     vk->copyBuffer(commandBuffer, stagingBuffer, indexBuffer, bufferSize);
@@ -114,5 +114,6 @@ void MeshAsset::render(VkCommandBuffer commandBuffer)
     VkBuffer vertexBuffers[] = {vertexBuffer};
     VkDeviceSize offsets[] = {0};
     vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
-    vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+    vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+    vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 }
