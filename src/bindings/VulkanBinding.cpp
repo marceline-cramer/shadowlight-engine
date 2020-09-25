1ec5b93c10491bb905f1b5e0114b61915aeec71a
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-09-24 20:16:48 -0600

    Decrease VulkanBinding validation verbosity

--- src/bindings/VulkanBinding.cpp
+++ src/bindings/VulkanBinding.cpp
@@ -121,8 +121,7 @@ void VulkanBinding::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreate
 {
     VkDebugUtilsMessengerCreateInfoEXT newCreateInfo = {
         .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
-        .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
-                           | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
+        .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
                            | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
         .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
                        | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
